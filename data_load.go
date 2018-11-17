de89afbb4238e8e733d4cae0d4d47fac62d30446
Brian Li <zhenbangli@gmail.com>
2018-11-17 01:10:45 -0800

    Refactor with zip read write util.

--- data_load.go
+++ data_load.go
@@ -1,18 +1,10 @@
 package redditngram
 
 import (
-	"bufio"
-	"compress/bzip2"
 	"encoding/json"
-	"io"
 	"log"
-	"os"
-	"path/filepath"
 	"strconv"
 	"strings"
-
-	"github.com/pierrec/lz4"
-	"github.com/ulikunitz/xz"
 )
 
 type Comment struct {
@@ -29,37 +21,20 @@ func LoadRedditComments(year, month int) (<-chan *Comment, error) {
 	datapath, err := GetRedditCommentsLocalPath(year, month)
 	if err != nil {
 		return nil, err
-	} else if _, err := os.Stat(datapath); os.IsNotExist(err) {
+	}
+
+	sender, err := ReadFromFileAuto(datapath)
+	if err != nil {
 		return nil, err
 	}
 
 	comments := make(chan *Comment)
 	go func() {
 		defer close(comments)
-		fh, err := os.Open(datapath)
-		if err != nil {
-			log.Fatalln(err)
-		}
-		defer fh.Close()
-
-		var reader io.Reader
-		switch filepath.Ext(datapath) {
-		case ".xz":
-			reader, err = xz.NewReader(fh)
-			if err != nil {
-				log.Fatalln(err)
-			}
-		case ".bz2":
-			reader = bzip2.NewReader(fh)
-		default:
-			log.Panic("Only .xz and .bz2 are support. Given: ", datapath)
-		}
-
-		scanner := bufio.NewScanner(reader)
 		var comment Comment
 		lineCount := 0
-		for scanner.Scan() {
-			json.Unmarshal([]byte(scanner.Text()), &comment)
+		for bytes := range sender {
+			json.Unmarshal(bytes, &comment)
 			if comment.Body != "[deleted]" {
 				comments <- &comment
 			}
@@ -79,35 +54,26 @@ func LoadRedditNgramCounts(year, month, order int) (counts map[string]uint64, er
 	datapath, err := GetRedditNgramCountsLocalPath(year, month, order)
 	if err != nil {
 		return nil, err
-	} else if _, err := os.Stat(datapath); os.IsNotExist(err) {
-		return nil, err
 	}
-	fh, err := os.Open(datapath)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer fh.Close()
-	reader := lz4.NewReader(fh)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	counts, err = LoadCountsFromTsv(reader)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return counts, nil
+	counts, err = LoadCountsFromTsv(datapath)
+	return counts, err
 }
 
-func LoadCountsFromTsv(r io.Reader) (counts map[string]uint64, err error) {
-	scanner := bufio.NewScanner(r)
+func LoadCountsFromTsv(path string) (counts map[string]uint64, err error) {
+	sender, err := ReadFromFileAuto(path)
+	if err != nil {
+		return nil, err
+	}
+
 	fields := make([]string, 2)
 	counts = make(map[string]uint64)
-	for scanner.Scan() {
-		fields = strings.Split(scanner.Text(), "\t")
+	for bytes := range sender {
+		fields = strings.Split(string(bytes), "\t")
 		counts[fields[0]], err = strconv.ParseUint(fields[1], 10, 64)
 		if err != nil {
 			return nil, err
 		}
 	}
+
 	return counts, nil
 }
